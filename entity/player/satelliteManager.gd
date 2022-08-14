18afacd07c769781b6fdc123bc6bf7c490394dc8
I'm Pen <88584656+Pencoding1@users.noreply.github.com>
2022-08-14 19:01:50 +0700

    add game interface
    Add game interface for all stage.

--- entity/player/satelliteManager.gd
+++ entity/player/satelliteManager.gd
@@ -1,34 +1,58 @@
+tool
 extends Node2D
 
-var orbs :Array
+var statellites :Array
+#Use camera2D "smoothing" feature to simulate momentum of some statelites.
+onready var tween :Camera2D = $tween
 
 export (Resource) var bullet
 export (Resource) var bullet_focus
-export (Resource) var orb_data
+export (Resource) var data
+export (PackedScene) var statellite : PackedScene
+export (int, "power 1", "power 2", "power 3", "power 4") var preset
 
-func orb_repos() -> void:
+export (bool) var save setget _save
+
+func _ready():
+	var node = statellite.instance()
+	add_child(node)
+	statellites.append(node)
+
+func _save(_value:bool) -> void:
+	if not Engine.editor_hint:
+		return
+	
+	var size := statellites.size()
+	var index : int
+	if size == 1:
+		index = 0
+	elif size == 2:
+		index = 1
+	elif size == 3:
+		index = 3
+	else:
+		index = 6
+		
+	for node in statellites:
+		data.positions[index] = node.position
+		data.rotations[index] = node.rotation
+		index += 1
+
+func load_preset(preset:int) -> void:
 	var orb_pos : Array
 	var orb_rot : Array
-	if orbs.size() == 1:
-		Global.tween.interpolate_property(orbs[0], orbs[0].position, orbs[0].position, orb_data.orb1_pos, 1)
-		return
-	elif orbs.size() == 2:
-		orb_pos = orb_data.orb2_pos
-		orb_rot = orb_data.orb2_rot
-	elif orbs.size() == 3:
-		orb_pos = orb_data.orb3_pos
-		orb_rot = orb_data.orb3_rot
-	else:
-		orb_pos = orb_data.orb4_pos
-		orb_pos = orb_data.orb4_rot
-	
 	var index := 0
-	for orb_node in orbs:
-		Global.tween.interpolate_property(orb_node, orb_node.position, orb_node.position, orb_pos[index], 1)
-		Global.tween.interpolate_property(orb_node, orb_node.rotation, orb_node.rotation, orb_rot[index], 1)
+	
+	if preset == 1:
+		index = 0
+	elif preset == 2:
+		index = 1
+	elif preset == 3:
+		index = 3
+	else:
+		index = 6
+	
+	for node in statellites:
+		Global.tween.interpolate_property(node, node.position, node.position, orb_pos[index], 1)
+		Global.tween.interpolate_property(node, node.rotation, node.rotation, orb_rot[index], 1)
 		index += 1
-	
-func add
-	
-func attack() -> void:
-	pass
