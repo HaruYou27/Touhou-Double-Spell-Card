61eea228554b26893aa2c995440499de9f70e0c0
Haruyou <68549456+haruyou27@users.noreply.github.com>
2022-08-21 15:17:37 +0700

    untested seeker & ricochet, wip item manager

--- entity/Bullet/Seeker.cs
+++ entity/Bullet/Seeker.cs
@@ -2,6 +2,26 @@ using Godot;
 
 public class Seeker : BulletBasic {
     [Export] protected float mass = 2;
+    [Export] protected float seekRadius {
+        set {Physics2DServer.ShapeSetData(seekShape, value);}
+        get {return (float)Physics2DServer.ShapeGetData(seekShape);}
+    }
+    [Export(PropertyHint.Layers2dPhysics)] uint seekLayer {
+        set {seekQuery.CollisionLayer = value;}
+        get {return seekQuery.CollisionLayer;}
+    }
+    [Export] bool seekAreas {
+        set {seekQuery.CollideWithAreas = value;}
+        get {return seekQuery.CollideWithAreas;}
+    }
+    [Export] bool seekBodies {
+        set {seekQuery.CollideWithBodies = value;}
+        get {return seekQuery.CollideWithBodies;}
+    }
+
+    private Bullet[] bullets;
+    protected Physics2DShapeQueryParameters seekQuery = new Physics2DShapeQueryParameters();
+    private RID seekShape = Physics2DServer.CircleShapeCreate();
     private struct Bullet {
         public Transform2D transform;
         public Vector2 velocity;
@@ -15,17 +35,9 @@ public class Seeker : BulletBasic {
         }
     }
 
-    public virtual void FindNearestNode(in Node2D[] list, in Vector2 relativeNode) {
-        float minDistance = float.PositiveInfinity;
-        Node2D theChosenOne = null;
-        foreach (Node2D node in list) {
-            float distance = (node.GlobalPosition - relativeNode).Length();
-            if (minDistance > distance) {
-                minDistance = distance;
-                theChosenOne = node;
-            }
-        }
-        target = theChosenOne;
+    public override void _EnterTree()
+    {
+        bullets = new Bullet[poolSize];
     }
     public override void _PhysicsProcess(float delta) {
         if (heat > 0) {heat--;}
@@ -33,7 +45,7 @@ public class Seeker : BulletBasic {
             foreach (Node2D barrel in barrels) {
 			if (index == poolSize) {return;}
 
-			Bullet bullet = new Bullet(speed, barrel.GlobalTransform);
+			Bullet bullet = new Bullet(speed, barrel.GlobalTransform, sprites.Pop());
 			bullets[index] = bullet;
 			index++;
 		    }
@@ -45,13 +57,20 @@ public class Seeker : BulletBasic {
 
         for (uint i = 0; i != index; i++) {
             Bullet bullet = bullets[i];
-            if (target != null) {
-                Vector2 desiredV = (target.GlobalPosition - bullet.transform.origin).Normalized() * speed;
+            if (bullet.target == null) {
+                seekQuery.Transform = bullet.transform;
+                Godot.Collections.Dictionary seekResult = world.DirectSpaceState.GetRestInfo(seekQuery);
+                if (seekResult.Count > 0) {
+                    bullet.target = (Node2D)GD.InstanceFromId((ulong) (int)seekResult["collider_id"]);
+                }
+            } else {
+                Vector2 desiredV = (bullet.target.GlobalPosition - bullet.transform.origin).Normalized() * speed;
                 bullet.velocity += (desiredV - bullet.velocity) / mass;
                 bullet.transform.Rotation = bullet.velocity.Angle();
             }
             bullet.transform.origin += bullet.velocity * delta;
             VisualServer.CanvasItemSetTransform(bullet.sprite, bullet.transform);
+
 
             //Collision checking
             query.Transform = bullet.transform;
