b2ac31d66272f29fb9ddce97bd61a585d47e577e
Haruyou27 <emaildai27@gmail.com>
2022-11-14 21:53:02 +0700

    wip main menu

--- user-interface/mainMenu/Menu.gd
+++ user-interface/mainMenu/Menu.gd
@@ -1,11 +1,12 @@
-extends Camera2D
+extends Control
 
 const ani_length := .15
 
-onready var settings := $"../setting"
-onready var characters := $"../character"
-onready var levels := $"../level"
-onready var main := $"../main"
+onready var settings := $setting
+onready var characters := $character
+onready var levels := $level
+onready var main := $main
+onready var camera := $Camera2D
 
 func _ready():
 	only_main()
@@ -17,21 +18,21 @@ func only_main():
 
 func _on_settings_pressed():
 	var tween := create_tween()
-	tween.ween_property(self, 'position', Vector2(-1280, 0), ani_length)
+	tween.ween_property(camera, 'position', Vector2(-1280, 0), ani_length)
 	tween.connect("finished", main, 'hide')
 
 func _on_back_pressed():
 	var tween := create_tween()
-	tween.tween_property(self, 'position', Vector2.ZERO, ani_length)
+	tween.tween_property(camera, 'position', Vector2.ZERO, ani_length)
 	tween.connect("finished", self, 'only_main')
 
 func select_character():
 	var tween := create_tween()
-	tween.tween_property(self, 'position', Vector2(1280, 0), ani_length)
+	tween.tween_property(camera, 'position', Vector2(1280, 0), ani_length)
 	tween.connect("finished", main, 'hide')
 
 func select_level():
 	var tween := create_tween()
-	tween.tween_property(self, 'position', Vector2(2560, 0), ani_length)
+	tween.tween_property(camera, 'position', Vector2(2560, 0), ani_length)
 	tween.connect("finished", main, 'hide')
 	tween.connect("finished", characters, 'hide')
